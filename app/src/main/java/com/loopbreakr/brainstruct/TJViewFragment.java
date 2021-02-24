282b7ab6f477659ccf56488d94ad6cafde209ec5
mkercode <mkercode@gmail.com>
2021-02-24 02:52:47 -0500

    created thought journal fragment

--- app/src/main/java/com/loopbreakr/brainstruct/TJViewFragment.java
+++ app/src/main/java/com/loopbreakr/brainstruct/TJViewFragment.java
@@ -2,57 +2,28 @@ package com.loopbreakr.brainstruct;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link TJViewFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class TJViewFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private Toolbar backToolbar;
 
     public TJViewFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ThoughtJournalFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static TJViewFragment newInstance(String param1, String param2) {
-        TJViewFragment fragment = new TJViewFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
@@ -61,4 +32,29 @@ public class TJViewFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_thought_journal, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        setBackToolBar(view);
+    }
+
+    private void setBackToolBar(View view) {
+        backToolbar = view.findViewById(R.id.back_toolbar);
+
+        ((AppCompatActivity)getActivity()).setSupportActionBar(backToolbar);
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setDisplayShowHomeEnabled(true);
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        backToolbar.setNavigationIcon(R.drawable.ic_arrow_back);
+        backToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().onBackPressed();
+            }
+        });
+    }
+
+
 }
