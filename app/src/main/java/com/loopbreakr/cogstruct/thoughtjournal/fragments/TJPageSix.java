6140b575d1a52871990f7bcc1456ad7922f07f1a
mkercode <mkercode@gmail.com>
2021-03-16 23:21:35 -0400

    updated tj_fragment_page_six.xml

--- app/src/main/java/com/loopbreakr/cogstruct/thoughtjournal/fragments/TJPageSix.java
+++ app/src/main/java/com/loopbreakr/cogstruct/thoughtjournal/fragments/TJPageSix.java
@@ -82,7 +82,7 @@ public class TJPageSix extends Fragment{
 
         submitButton.setOnClickListener(v ->{
             if(!requiredInputs.contains("") &&!requiredInputs.contains(null) && !tjViewModel.getThoughtList().isEmpty()){
-//                submitData();
+                submitData();
                 Toast.makeText(requireActivity().getApplicationContext(), "Saved in logs", Toast.LENGTH_SHORT).show();
                 Intent intent = new Intent(this.requireActivity(), MainActivity.class);
                 startActivity(intent);
