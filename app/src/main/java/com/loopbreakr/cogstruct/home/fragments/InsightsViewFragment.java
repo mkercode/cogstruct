1b2f4fafdc1f7e9af6244fff3589ad553bbb787d
mkercode <mkercode@gmail.com>
2021-04-13 09:09:05 -0400

    added functionality for insights view fragments

--- app/src/main/java/com/loopbreakr/cogstruct/home/fragments/InsightsViewFragment.java
+++ app/src/main/java/com/loopbreakr/cogstruct/home/fragments/InsightsViewFragment.java
@@ -2,7 +2,14 @@ package com.loopbreakr.cogstruct.home.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,8 +17,12 @@ import android.view.ViewGroup;
 
 import com.loopbreakr.cogstruct.R;
 
+import java.util.Objects;
+
 
 public class InsightsViewFragment extends Fragment {
+    private NavController navController;
+    private CardView behavioralInspectionCard, gamePlanCard, findThinkingErrorsCard;
 
     public InsightsViewFragment() {
         // Required empty public constructor
@@ -30,4 +41,38 @@ public class InsightsViewFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.home_fragment_insights_view, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        navController = Navigation.findNavController(view);
+        setBackToolBar(view);
+        findViews(view);
+        setListenerBehavior();
+    }
+
+    private void setBackToolBar(View view) {
+        Toolbar backToolbar = view.findViewById(R.id.back_toolbar);
+
+        ((AppCompatActivity)requireActivity()).setSupportActionBar(backToolbar);
+        Objects.requireNonNull(((AppCompatActivity) requireActivity()).getSupportActionBar()).setDisplayShowTitleEnabled(false);
+
+        backToolbar.setNavigationIcon(R.drawable.ic_arrow_back);
+        backToolbar.setNavigationOnClickListener(v -> requireActivity().onBackPressed());
+    }
+
+    private void findViews(View view) {
+
+        behavioralInspectionCard= view.findViewById(R.id.behavioral_inspection_card);
+        gamePlanCard = view.findViewById(R.id.game_plan_card);
+        findThinkingErrorsCard = view.findViewById(R.id.find_thinking_errors_card);
+    }
+
+    private void setListenerBehavior(){
+        behavioralInspectionCard.setOnClickListener(v -> navController.navigate(R.id.action_insightsViewFragment_to_BIViewFragment));
+        gamePlanCard.setOnClickListener(v -> navController.navigate(R.id.action_insightsViewFragment_to_GPViewFragment));
+        findThinkingErrorsCard.setOnClickListener(v -> navController.navigate(R.id.action_insightsViewFragment_to_FTEViewFragment));
+    }
+
+
 }
