df4e65b8fa2ba04ca948ce986e73a6153a205d2d
mkercode <mkercode@gmail.com>
2021-04-14 00:57:42 -0400

    created nav graph, activity, and fragments for behavioral inspection

---
+++ app/src/main/java/com/loopbreakr/cogstruct/behavioralinspection/BIActivity.java
@@ -0,0 +1,16 @@
+package com.loopbreakr.cogstruct.behavioralinspection;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.loopbreakr.cogstruct.R;
+
+public class BIActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bi_activity);
+    }
+}
